c0139bf136c3efaaedb8f6cdb720c5f26edc0ff6
lbarberiscanoni <hllbck7@gmail.com>
2015-08-30 17:09:33 -0700

    fixed the update functionality so that now you can a new trailer if needed

--- main.js
+++ main.js
@@ -2,20 +2,21 @@ var trailerDB = new Firebase("https://netflix-trailers.firebaseio.com/trailers2"
 
 $(document).ready(function() {
     trailerDB.on("child_added", function(snapshot) {
-        var trailerTitle = snapshot.key();
-        var trailerLink = snapshot.val();
+        var trailer = snapshot.val();
+        var trailerTitle = trailer.title;
+        var trailerLink = trailer.link;
+        var trailerID = trailer.id;
         $(".container").append("<a>" + trailerTitle + "</a><br>");
         $(".container a:last").addClass("btn btn-default").attr("id", "trailer");
         $(".container #trailer:last").click(function() {
-            var thisID = $(this).text();
             $("<a class='btn btn-default' id='watch'>" + "Watch" + "</a>").insertAfter(this);
             $("<a class='btn btn-default' id='change'>" + "Change" + "</a>").insertAfter(this);
             $("<div class='col-md-4'><input type='text' class='form-control' placeholder='Enter new URL' id='newURL'></input></div>").insertAfter(this);
             $("#watch").attr("href", trailerLink).attr("target", "blank");
             $("#change").click(function() {
                 var newURL = $("#newURL").val();
-                trailerDB.child(trailerTitle).push({
-                    trailerTitle: newURL,
+                trailerDB.child(trailerID).update({
+                    "link": newURL,
                 });
                 window.location.reload();
             });
